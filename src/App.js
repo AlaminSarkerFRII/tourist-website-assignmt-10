31f09c06d8b438663734d8226a4254d7292290d0
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-04-16 23:25:27 +0600

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./components/Pages/About/About";
+import CheckOut from "./components/Pages/CheckOut/CheckOut";
+import Home from "./components/Pages/Home/Home/Home";
+import Login from "./components/Pages/Login/Login/Login";
+import Register from "./components/Pages/Login/Register/Register";
+import NotFound from "./components/Shared/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <h2 className="text-info">Visit world With Me</h2>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/checkout" element={<CheckOut></CheckOut>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Pages/Login/RequireAuth/RequireAuth.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RequireAuth = () => {
+  return (
+    <div>
+      <h2>Required Auth pages</h2>
+    </div>
+  );
+};
+
+export default RequireAuth;
